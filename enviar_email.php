5ad5b5a2c1a28768f68a0fdd08c98005b71d0fa2
Gustavo Santos <133147233+GsantosBr12@users.noreply.github.com>
2023-10-24 23:16:48 -0300

    enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -1,14 +1,22 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["name"];
+    $nome = $_POST["nome"];
     $email = $_POST["email"];
-    $mensagem = $_POST["message"];
+    $mensagem = $_POST["mensagem"];
     
-    $para = "12201987@aluno.cotemig.com.br"; // Substitua com o seu endereço de e-mail
+    // Destinatário
+    $destinatario = "12201986@aluno.cotemig.com.br"; // Substitua pelo seu endereço de e-mail
+
+    // Assunto
     $assunto = "Nova mensagem de $nome";
-    $headers = "De: $email";
-    
-    mail($para, $assunto, $mensagem, $headers);
+
+    // Cabeçalhos
+    $headers = "De: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    // Envie o e-mail
+    mail($destinatario, $assunto, $mensagem, $headers);
     
     echo "Mensagem enviada com sucesso!";
 } else {
